b382ca37cf62764aeaa339ab0d4947765cdc4a19
Kirill Volyk <200638948@student.georgianc.on.ca>
2026-05-25 20:32:29 -0400

    Made 2 model classes that have one to many relationship, did navigation(i hope)

---
+++ RoyalPawsProcuts/Models/Owner.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RoyalPawsProcuts.Models
+{
+    public class Owner
+    {
+        public int OwnerId { get; set; } // Primary key
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        // Navigation: One Owner to Many Pets
+        public List<Pets> Pets { get; set; } = new();
+    }
+}
---
+++ RoyalPawsProcuts/Models/Pets.cs
@@ -0,0 +1,14 @@
+﻿namespace RoyalPawsProcuts.Models
+{
+    public class Pets
+    {
+        public int PetId { get; set; } // Primary key
+        public string PetName { get; set; }
+        public string Species { get; set; }
+        public string Breed { get; set; }
+        public int OwnerId { get; set; } // Foreign key to Owner
+
+        // Navigation: Many Pets to One Owner
+        public Pets Pet { get; set; }
+    }
+}
